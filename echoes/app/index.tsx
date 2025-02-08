c2b944dcc5ce185ebb0d0d45da1eb37c8c126ecc
Eric Guan <ericguan2004@gmail.com>
2025-02-08 16:43:04 -0500

    Navigator basic

--- echoes/app/index.tsx
+++ echoes/app/index.tsx
@@ -1,6 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import Navigator from './navigation/Navigator';
 
-// Register the main app component
-AppRegistry.registerComponent(appName, () => App);
+export default function App() {
+  return <Navigator />;
+}
